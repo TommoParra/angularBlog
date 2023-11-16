52ac91416bf417000c7485932c02bbee0a461fec
TommoParra <tomas.moralparra@gmail.com>
2023-11-16 10:02:18 +0100

    scroll service, header's on-scroll animation, basic css

--- src/app/services/posts.service.ts
+++ src/app/services/posts.service.ts
@@ -140,7 +140,7 @@ export class PostsService {
 
     return postsWithFeaturedMoved.map(post => ({
       ...post,
-      text: this.truncateText(post.text, post.featured ? 800 : 300)
+      text: this.truncateText(post.text, post.featured ? 700 : 200)
     }));
   }
 }
